b483211d8afaaae4f8ca95b614372afe8a044a74
Davi <Davi@DESKTOP-C6MJUKL>
2020-09-04 00:05:53 -0300

    feat: initial structure.

---
+++ Assets/Scripts/Editor/AutoSaveExtension.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSaveExtension
+{
+    static AutoSaveExtension()
+    {
+        EditorApplication.playModeStateChanged += AutoSaveWhenPlaymodeStarts;
+    }
+
+    private static void AutoSaveWhenPlaymodeStarts(PlayModeStateChange playModeStateChange)
+    {
+        if (playModeStateChange == PlayModeStateChange.ExitingEditMode)
+        {
+            // Save the scene and the assets.
+            EditorSceneManager.SaveOpenScenes();
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
